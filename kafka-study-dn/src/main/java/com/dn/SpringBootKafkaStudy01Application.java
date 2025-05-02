6158bf55ed39706da3a6f2ec804e8cb6eba5265f
itorly <itorly@163.com>
2025-05-02 21:47:20 +0800

    application class

---
+++ kafka-study-dn/src/main/java/com/dn/SpringBootKafkaStudy01Application.java
@@ -0,0 +1,13 @@
+package com.dn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaStudy01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaStudy01Application.class, args);
+    }
+
+}
