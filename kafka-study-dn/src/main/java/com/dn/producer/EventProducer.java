c126b5709371f73abaee043d282ec9700076bac7
itorly <itorly@163.com>
2025-05-03 17:31:43 +0800

    sendEventByProducerRecord

--- kafka-study-dn/src/main/java/com/dn/producer/EventProducer.java
+++ kafka-study-dn/src/main/java/com/dn/producer/EventProducer.java
@@ -1,11 +1,16 @@
 package com.dn.producer;
 
 import jakarta.annotation.Resource;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeaders;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class EventProducer {
@@ -23,5 +28,22 @@ public class EventProducer {
                         .setHeader(KafkaHeaders.TOPIC, topicName)
                         .build();
         kafkaTemplate.send(stringMessage);
+    }
+
+    public void sendEventByProducerRecord(String topicName, String string) {
+        Headers headers = new RecordHeaders();
+        headers.add("k1", "v1".getBytes());
+        headers.add("orderId", "1234567890".getBytes(StandardCharsets.UTF_8));
+
+        //  String topic, Integer partition, Long timestamp, K key, V value, Iterable<Header> headers
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(
+                topicName,
+                0,
+                System.currentTimeMillis(),
+                "key1",
+                string,
+                headers
+        );
+        kafkaTemplate.send(producerRecord);
     }
 }
