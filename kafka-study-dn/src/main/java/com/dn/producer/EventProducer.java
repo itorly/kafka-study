9f1d187fd8f30ca303de20dcc076ac6d1154958c
itorly <itorly@163.com>
2025-05-03 17:09:10 +0800

    sendEventByMessage

--- kafka-study-dn/src/main/java/com/dn/producer/EventProducer.java
+++ kafka-study-dn/src/main/java/com/dn/producer/EventProducer.java
@@ -2,6 +2,9 @@ package com.dn.producer;
 
 import jakarta.annotation.Resource;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,5 +15,13 @@ public class EventProducer {
 
     public void sendEvent(String topic, String message) {
         kafkaTemplate.send(topic, message);
+    }
+
+    public void sendEventByMessage(String topicName, String string) {
+        Message<String> stringMessage =
+                MessageBuilder.withPayload(string)
+                        .setHeader(KafkaHeaders.TOPIC, topicName)
+                        .build();
+        kafkaTemplate.send(stringMessage);
     }
 }
