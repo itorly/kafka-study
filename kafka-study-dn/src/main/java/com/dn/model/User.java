b02d398feecc22e108063088f2304a194a6d94bb
itorly <itorly@163.com>
2025-05-04 23:58:12 +0800

    User.java

---
+++ kafka-study-dn/src/main/java/com/dn/model/User.java
@@ -0,0 +1,23 @@
+package com.dn.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User implements Serializable {
+
+    private int id;
+
+    private String phone;
+
+    private Date birthDay;
+
+}
