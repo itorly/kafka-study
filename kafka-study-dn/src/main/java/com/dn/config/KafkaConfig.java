e6399ec95e14dad840f967a9d600f18a7f7267f0
itorly <itorly@163.com>
2025-05-06 12:49:13 +0800

    Call the function to initialize the topic name, the number of partitions and the number of replicas.

---
+++ kafka-study-dn/src/main/java/com/dn/config/KafkaConfig.java
@@ -0,0 +1,14 @@
+package com.dn.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic newTopic() {
+        return new NewTopic("he-topic", 5, (short) 1);
+    }
+}
