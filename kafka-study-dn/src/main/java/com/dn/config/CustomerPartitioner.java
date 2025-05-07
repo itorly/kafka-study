44b2a01d2df0aa31d46a831711918c153a7059db
itorly <itorly@163.com>
2025-05-07 23:59:31 +0800

    CustomerPartitioner

---
+++ kafka-study-dn/src/main/java/com/dn/config/CustomerPartitioner.java
@@ -0,0 +1,42 @@
+package com.dn.config;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.utils.Utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CustomerPartitioner implements Partitioner {
+
+    private AtomicInteger nextPartition = new AtomicInteger(0);
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+
+        if (key == null) {
+            int next = nextPartition.getAndIncrement();
+            if (next >= numPartitions) {
+                nextPartition.compareAndSet(next, 0);
+            }
+            System.out.println("分区值：" + next);
+            return next;
+        } else {
+            return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
