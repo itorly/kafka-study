f26ba1162add3c2f785d78967289419f1a780f6c
itorly <itorly@163.com>
2025-05-07 23:15:17 +0800

    Producer-related configuration

--- kafka-study-dn/src/main/java/com/dn/config/KafkaConfig.java
+++ kafka-study-dn/src/main/java/com/dn/config/KafkaConfig.java
@@ -1,11 +1,40 @@
 package com.dn.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    /**
+     * Producer-related configuration
+     */
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>(6);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
+        return props;
+     }
+
 
     //  Send a message to heTopic.
     //  After restarting the project, the messages in the heTopic partition will not be lost.
