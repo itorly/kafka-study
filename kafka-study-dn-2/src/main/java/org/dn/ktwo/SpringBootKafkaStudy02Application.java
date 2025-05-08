f71a6429caef5c1b7bb749dae2d35a23d553f5c7
itorly <itorly@163.com>
2025-05-08 23:47:46 +0800

    main startup function

---
+++ kafka-study-dn-2/src/main/java/org/dn/ktwo/SpringBootKafkaStudy02Application.java
@@ -0,0 +1,11 @@
+package org.dn.ktwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaStudy02Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaStudy02Application.class, args);
+    }
+}
