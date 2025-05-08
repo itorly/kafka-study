b2b3bd00550fc7b141e2ae3054ad22ed90391f1f
itorly <itorly@163.com>
2025-05-08 23:58:24 +0800

    EventProducer

---
+++ kafka-study-dn-2/src/main/java/org/dn/ktwo/producer/EventProducer.java
@@ -0,0 +1,17 @@
+package org.dn.ktwo.producer;
+
+import jakarta.annotation.Resource;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventProducer {
+
+    @Resource
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendEvent(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+    }
+
+}
