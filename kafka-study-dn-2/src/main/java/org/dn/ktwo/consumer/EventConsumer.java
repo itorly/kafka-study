2c946584595ae2982346ae6cd1514b7fbd17c34e
itorly <itorly@163.com>
2025-05-09 18:09:15 +0800

    onEventWhenParameterIsJsonString

--- kafka-study-dn-2/src/main/java/org/dn/ktwo/consumer/EventConsumer.java
+++ kafka-study-dn-2/src/main/java/org/dn/ktwo/consumer/EventConsumer.java
@@ -1,6 +1,8 @@
 package org.dn.ktwo.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.dn.ktwo.model.User;
+import org.dn.ktwo.util.JSONUtils;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -8,7 +10,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EventConsumer {
+public class EventConsumer<T> {
 
     /**
      * Receive messages or data by means of listening.
@@ -18,7 +20,7 @@ public class EventConsumer {
      * @param topic
      * @param partition
      */
-    @KafkaListener(topics = {"helloTopic"}, groupId = "helloGroup")
+//    @KafkaListener(topics = {"helloTopic"}, groupId = "helloGroup")
     public void onEvent(
             @Payload String event,
             @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
@@ -28,6 +30,22 @@ public class EventConsumer {
         System.out.println("EventConsumer onEvent 1: " + event +
                 ", topic : " + topic +
                 ", partition : " + partition);
+        System.out.println("consumerRecord : " + consumerRecord.toString());
+    }
+
+    @KafkaListener(topics = {"helloTopic"}, groupId = "helloGroup")
+    public void onEventWhenParameterIsJsonString(
+            String jsonString,
+//            Class<T> clazz,
+            @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+            @Header(value = KafkaHeaders.RECEIVED_PARTITION) String partition,
+            ConsumerRecord<String, String> consumerRecord) {
+//        T t = JSONUtils.toBean(jsonString, clazz);
+        User user = JSONUtils.toBean(jsonString, User.class);
+        System.out.println("EventConsumer onEvent 1: " + jsonString +
+                ", \nuser : " + user +
+                ", \ntopic : " + topic +
+                ", \npartition : " + partition);
         System.out.println("consumerRecord : " + consumerRecord.toString());
     }
 }
