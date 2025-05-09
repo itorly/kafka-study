247fcbb3dd8a72bd45ae10591fa7b436be40c2ec
itorly <itorly@163.com>
2025-05-10 00:00:39 +0800

    Acknowledgment

--- kafka-study-dn-2/src/main/java/org/dn/ktwo/consumer/EventConsumer.java
+++ kafka-study-dn-2/src/main/java/org/dn/ktwo/consumer/EventConsumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.dn.ktwo.model.User;
 import org.dn.ktwo.util.JSONUtils;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -49,17 +50,34 @@ public class EventConsumer<T> {
         System.out.println("consumerRecord : " + consumerRecord.toString());
     }
 
-    @KafkaListener(topics = {"${kafka.topic.name}"}, groupId = "${kafka.consumer.group}")
+//    @KafkaListener(topics = {"${kafka.topic.name}"}, groupId = "${kafka.consumer.group}")
     public void onEvent3(
             String jsonString,
             @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
             @Header(value = KafkaHeaders.RECEIVED_PARTITION) String partition,
-            ConsumerRecord<String, String> consumerRecord) {
+            @Payload ConsumerRecord<String, String> consumerRecord) {
         User user = JSONUtils.toBean(jsonString, User.class);
         System.out.println("EventConsumer onEvent 3: " + jsonString +
                 ", \nuser : " + user +
                 ", \ntopic : " + topic +
                 ", \npartition : " + partition);
         System.out.println("consumerRecord : " + consumerRecord.toString());
+    }
+
+    @KafkaListener(topics = {"${kafka.topic.name}"}, groupId = "${kafka.consumer.group}")
+    public void onEvent4(
+            String jsonString,
+            @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+            @Header(value = KafkaHeaders.RECEIVED_PARTITION) String partition,
+            @Payload ConsumerRecord<String, String> consumerRecord,
+            Acknowledgment ack) {
+        User user = JSONUtils.toBean(jsonString, User.class);
+        System.out.println("EventConsumer onEvent 4: " + jsonString +
+                ", \nuser : " + user +
+                ", \ntopic : " + topic +
+                ", \npartition : " + partition);
+        System.out.println("consumerRecord : " + consumerRecord.toString());
+
+        ack.acknowledge();
     }
 }
