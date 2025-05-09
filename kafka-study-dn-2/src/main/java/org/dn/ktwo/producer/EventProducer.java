935c243274b7c7146d242a3cfe3cedd67b03fa10
itorly <itorly@163.com>
2025-05-09 18:09:34 +0800

    sendEventWhenParameterIsBean

--- kafka-study-dn-2/src/main/java/org/dn/ktwo/producer/EventProducer.java
+++ kafka-study-dn-2/src/main/java/org/dn/ktwo/producer/EventProducer.java
@@ -1,6 +1,8 @@
 package org.dn.ktwo.producer;
 
 import jakarta.annotation.Resource;
+import org.dn.ktwo.model.User;
+import org.dn.ktwo.util.JSONUtils;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +12,16 @@ public class EventProducer {
     @Resource
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Resource
+    private KafkaTemplate<String, Object> kafkaTemplate2;
+
     public void sendEvent(String topic, String message) {
         kafkaTemplate.send(topic, message);
+    }
+
+    public void sendEventWhenParameterIsBean(User user) {
+        String userJSON = JSONUtils.toJSON(user);
+        kafkaTemplate.send("helloTopic", userJSON);
     }
 
 }
