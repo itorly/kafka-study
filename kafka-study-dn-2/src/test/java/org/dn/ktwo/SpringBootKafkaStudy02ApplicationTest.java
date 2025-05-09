4cbca898b10c1af01edcf138f23f648cdd74137a
itorly <itorly@163.com>
2025-05-09 18:09:49 +0800

    test sendEventWhenParameterIsBean

--- kafka-study-dn-2/src/test/java/org/dn/ktwo/SpringBootKafkaStudy02ApplicationTest.java
+++ kafka-study-dn-2/src/test/java/org/dn/ktwo/SpringBootKafkaStudy02ApplicationTest.java
@@ -1,9 +1,12 @@
 package org.dn.ktwo;
 
 import jakarta.annotation.Resource;
+import org.dn.ktwo.model.User;
 import org.dn.ktwo.producer.EventProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 public class SpringBootKafkaStudy02ApplicationTest {
@@ -13,5 +16,16 @@ public class SpringBootKafkaStudy02ApplicationTest {
     @Test
     public void testSendEvent() {
         eventProducer.sendEvent("helloTopic", "hello kafka");
+    }
+
+    @Test
+    public void testSendEventWhenParameterIsBean() {
+        User user = User.builder().
+                id(1209).
+                phone("13709090910").
+                birthDay(new Date()).
+                build();
+
+        eventProducer.sendEventWhenParameterIsBean(user);
     }
 }
