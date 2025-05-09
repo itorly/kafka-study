272db03810f13cc0512b87b77adfdce818939260
itorly <itorly@163.com>
2025-05-09 16:40:19 +0800

    @Payload, @Header

--- kafka-study-dn-2/src/main/java/org/dn/ktwo/consumer/EventConsumer.java
+++ kafka-study-dn-2/src/main/java/org/dn/ktwo/consumer/EventConsumer.java
@@ -1,14 +1,30 @@
 package org.dn.ktwo.consumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EventConsumer {
 
-    //  Receive messages or data by means of listening.
+    /**
+     * Receive messages or data by means of listening.
+     * @Payload: Mark this parameter as the content of the message body.
+     * @Header: Mark this parameter as the content of the message header.
+     * @param event
+     * @param topic
+     * @param partition
+     */
     @KafkaListener(topics = {"helloTopic"}, groupId = "helloGroup")
-    public void onEvent(String eventStr) {
-        System.out.println("EventConsumer onEvent: " + eventStr);
+    public void onEvent(
+            @Payload String event,
+            @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+//            @Header(value = KafkaHeaders.RECEIVED_KEY) String key,
+            @Header(value = KafkaHeaders.RECEIVED_PARTITION) String partition) {
+        System.out.println("EventConsumer onEvent: " + event +
+                ", topic : " + topic +
+                ", partition : " + partition);
     }
 }
