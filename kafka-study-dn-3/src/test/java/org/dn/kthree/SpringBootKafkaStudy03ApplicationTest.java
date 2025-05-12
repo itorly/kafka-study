1a49fb97b9a250cdf1bcb38b116f1cb3c785714e
itorly <itorly@163.com>
2025-05-12 19:39:33 +0800

    test eventProducer.sendEvent()

--- kafka-study-dn-3/src/test/java/org/dn/kthree/SpringBootKafkaStudy03ApplicationTest.java
+++ kafka-study-dn-3/src/test/java/org/dn/kthree/SpringBootKafkaStudy03ApplicationTest.java
@@ -1,8 +1,19 @@
 package org.dn.kthree;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.Resource;
+import org.dn.kthree.producer.EventProducer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
+@SpringBootTest
 public class SpringBootKafkaStudy03ApplicationTest {
+
+    @Resource
+    private EventProducer eventProducer;
+
+    @Test
+    void test01() {
+        eventProducer.sendEvent();
+    }
 
 }
