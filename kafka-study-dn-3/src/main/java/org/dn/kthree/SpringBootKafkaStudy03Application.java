b7e40ce50410c8d2681ab0f50ce13a922583dbfd
itorly <itorly@163.com>
2025-05-12 18:56:49 +0800

    new module: kafka-study-dn-3

---
+++ kafka-study-dn-3/src/main/java/org/dn/kthree/SpringBootKafkaStudy03Application.java
@@ -0,0 +1,11 @@
+package org.dn.kthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaStudy03Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaStudy03Application.class, args);
+    }
+}
---
+++ kafka-study-dn-3/src/test/java/org/dn/kthree/SpringBootKafkaStudy03ApplicationTest.java
@@ -0,0 +1,8 @@
+package org.dn.kthree;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaStudy03ApplicationTest {
+
+}
